b69f60b46c5cab7d1401c507f6557e994324bec1
wwwlir <wwwlir@gmail.com>
2016-02-15 18:39:39 +0800

    new

---
+++ z_TestFX/src/application/Main.java
@@ -0,0 +1,113 @@
+package application;
+	
+import java.util.ArrayList;
+
+import application.Model.Person;
+import application.View.PersonOverviewController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	private ObservableList<Person> personData = FXCollections.observableArrayList();
+	
+	{
+		personData.add(new Person("aaaa", "bbbbb"));
+		personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+	}
+	
+	public static void main(String[] args) {
+		
+		// Add some sample data
+        
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApps");
+		
+		
+		initRootLayout();
+		showPersonOverview();
+//		try {
+//			BorderPane root = new BorderPane();
+//			Scene scene = new Scene(root,400,400);
+//			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+	}
+	
+	/**
+     * Initializes the root layout.
+     */
+	public void initRootLayout(){
+		try {
+			// Load root layout from fxml file.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("View/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Show the scene containing the root layout.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	/**
+     * Shows the person overview inside the root layout.
+     */
+	
+	public void showPersonOverview(){
+		try {
+            // Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("View/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+            
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+	}
+	
+	/**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+    
+    public ObservableList<Person> getPersonData(){
+    	return personData;
+    }
+}
